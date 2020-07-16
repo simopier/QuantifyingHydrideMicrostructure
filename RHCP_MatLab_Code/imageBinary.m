37d4098b0a33d34f1f50b734e822a8d8701b47e4
Pierre-Clement Simon <pjs5523@psu.edu>
2020-07-16 08:24:01 -0400

    Update image binary function

--- RHCP_MatLab_Code/imageBinary.m
+++ RHCP_MatLab_Code/imageBinary.m
@@ -2,67 +2,74 @@ function [ binaryImage,lowThreshold, highThreshold, SpotSize, HoleSize, removeBo
 
 %-------------------------------------------------------------------------%
 %                                                                         %
-%       Script developed by Cailon Frank and Pierre-Clement Simon         %
-%       Penn State University                                             %
+%       Script developed by Pierre-Clement A Simon and Cailon Frank       %
+%       From Penn State University                                        %
+%                                                                         %
+%       Published in                                                      %
+%           Quantifying zirconium embrittlement due to hydride            %
+%           microstructure using image analysis                           %
+%           https:// ...                                                  %
+%                                                                         %
+%       Full MATLAB Code available at:                                    %
+%           https://github.com/simopier/QuantifyingHydrideMicrostructure  %
 %                                                                         %
 %-------------------------------------------------------------------------%
 
+% Description:
+% This function is called by imageBinary_folder.m and calls thershold.m,
+% the GUI function, to let the user select the appropriate binarization
+% parameters. imageBinary.m then binarizes the image using these parameters
+% and save the parameters and the binary image.
+% If the binary parameters were already determined and saved in the result
+% folder, then it will use these to binarize the figures again.
 
-%imageBinary takes in argument the filename and the name of the folder
-% containing this file, the starting low threshold, the starting high
-% threshold for the binarization of the image, and the size (in pixels) of
-% the spots that should be removed.
-% It also gives the option to show the initial and final images. It lets
-% the user define the low and high threshold values as well as the spot
-% size.
 
-%% OPEN, BINARIZE, AND CLEAN IMAGE
-% Binarizing the image is the first and one of the most important steps
-% of the algorithm. The aim is to keep only the hydrides in the image. The
-% user can be asked to validate the binary image after making sure that all
-% hydrides, and only hydrides, are present. The user can validate the
-% image, or change the parameters of the binarization process.
+% Inputs:
+% - codeFolderName: The name of the folder in which the RHF code is stored.
+% - imageFolderName: The name of the folder in which images are stored.
+% - filename: Name of the microstructure file.
+% - startingLowThreshold: The initial value for low threshold value ≥0 for the binarization process. To be adjusted in GUI. We recommend starting with 0.
+% - startingHighThreshold: The initial value for high threshold value >(startingLowThreshold) for the binarization process. To be adjusted in GUI. We recommend starting with 255.
+% - SpotSize: The binarization parameter to remove particles smaller than SpotSize in pixels. To be adjusted in GUI. We recommend starting with 0.
+% - HoleSize: The binarization parameter to fill out holes smaller than HoleSize in pixels. To be adjusted in GUI. We recommend starting with 0.
+% - resultsFolderName: The name of the folder in which the results will be stored. The folder is created if it does already exist.
 
-% The first argument of the imageBinary function is the name of the file
-% the user want to analyse. The second is the threshold value for the
-% binarization. The higher the threshold, the more pixels will be kept.
+% Outputs:
+% imageBinary.m returns the binary image as well as the binarization
+% parameters selected by the user. It also saves them in the result folder.
 
-% The second argument of the imageBinary function is the size of the
-% smallest group of pixels that will be kept. Any group of pixels with a
-% lower number of pixels that this value will be deleted in the picture.
-% This is useful to remove the noise or some impurities in the image.
-% The parameters defined during this step will be used for the rest of the
-% images
 
-% Check that user has the Image Processing Toolbox installed.
+%%%%%%% Check if that user has the Image Processing Toolbox installed %%%%%
 hasIPT = license('test', 'image_toolbox');
 if ~hasIPT
-    % User does not have the toolbox installed.
-    message = sprintf('Sorry, but you do not seem to have the Image Processing Toolbox.\nDo you want to try to continue anyway (Not recommended, as this will not work)? \n To install the toolbox, if you have a recent version, on the main MATLAB window, go to __Home>Adds-Ons>Get Adds-Ons__ and find the Image processing toolbox. Otherwise, visit: https://www.mathworks.com/matlabcentral/answers/101885-how-do-i-install-additional-toolboxes-into-an-existing-installation-of-matlab');
-    reply = questdlg(message, 'Toolbox missing', 'Yes', 'No', 'Yes');
-    if strcmpi(reply, 'No')
-        % User said No, so exit.
-        return;
+	% User does not have the toolbox installed.
+	message = sprintf('Sorry, but you do not seem to have the Image Processing Toolbox.\nDo you want to try to continue anyway (Not recommended, as this will not work)? \n To install the toolbox, if you have a recent version, on the main MATLAB window, go to __Home>Adds-Ons>Get Adds-Ons__ and find the Image processing toolbox. Otherwise, visit: https://www.mathworks.com/matlabcentral/answers/101885-how-do-i-install-additional-toolboxes-into-an-existing-installation-of-matlab');
+	reply = questdlg(message, 'Toolbox missing', 'Yes', 'No', 'Yes');
+	if strcmpi(reply, 'No')
+		% User said No, so exit.
+		return;
     end
 end
 
-% Open the image
+%%%%%%%%%%%%%%%%%%%%%%%%%%%% Open the image %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 cd ../
 cd(ImageFolderName)
 I = imread(filename);
 cd ../
 cd(codeFolderName)
 
-% Copy this image in current folder to use it in threshold. It will be
-% deleted later
+%%%%%%%%% Copy this image in current folder to use it in threshold %%%%%%%%
+% It will be deleted later
 copyfile(['../' ImageFolderName '/' filename], ['../' codeFolderName ],'f')
 
+%%%%%%%%%%%%%%% Check if the image has already been binarized %%%%%%%%%%%%%
 % if the image was already binarized, we use old values, otherwise we
 % define new ones
 cd ../
 cd(resultsFolderName)
 filename_param=[filename '_BinaryParam' '.csv'];
 existfile= exist(filename_param,'file');
+
 if (existfile == 2 )
     tab=csvread(filename_param,1,0);
     lowThreshold=tab(1);
@@ -70,42 +77,38 @@ if (existfile == 2 )
     SpotSize=tab(3);
     HoleSize=tab(4);
     removeBorders=tab(5);
-	background_bool=tab(6);
-	background_value=tab(7);
+    background_bool=tab(6);
+    background_value=tab(7);
     cd ../
     cd (codeFolderName)
 else
     cd ../
     cd (codeFolderName)
-    % Use threshold to define the parameters of the binarization process
-		removeBorders = 0;
-		background_bool = 0;
-		background_value = 1;
-    [lowThreshold, highThreshold, SpotSize, HoleSize, removeBorders, background_bool, background_value ] = threshold(startingLowThreshold, startingHighThreshold, SpotSize, HoleSize, removeBorders, background_bool, background_value, filename);
+    % Use threshold to define the parameters of the binarization process %%
+    removeBorders = 0;
+    background_bool = 0;
+    background_value = 1;
+    [lowThreshold, highThreshold, SpotSize, HoleSize, removeBorders, background_bool, background_value  ] = threshold(startingLowThreshold, startingHighThreshold, SpotSize, HoleSize, removeBorders, background_bool, background_value, filename);
 end
-% SAVE THE RESULTS IN A FILE IN THE FOLDER RESULTS
+
+%%%%%%%%%%%%% Save the results in a .csv file in result folder %%%%%%%%%%%%
 cd ../
 cd(resultsFolderName)
-param_mat=[lowThreshold highThreshold SpotSize HoleSize removeBorders  background_bool background_value];
+param_mat=[lowThreshold highThreshold SpotSize HoleSize removeBorders background_bool background_value];
 % add a header
-cHeader = {'low_Threshold' 'high_Threshold' 'Spot_size' 'Hole_size' 'Remove_Borders'  'Non_uniform_background' 'Non_uniform_background_value'}; %header
+cHeader = {'low_Threshold' 'high_Threshold' 'Spot_size' 'Hole_size' 'Remove_Borders' 'Non_uniform_background' 'Non_uniform_background_value'}; %header
 textHeader = strjoin(cHeader, ',');
 % write header to file
 fid = fopen(filename_param,'w');
 fprintf(fid,'%s\n',textHeader);
 fclose(fid);
-
 % write data in file
 dlmwrite(filename_param,param_mat,'-append');
 cd ../
 cd (codeFolderName)
 
 
-%binarize the image, and take the complement of it (white pixels represents
-%the hydride in binaryImage, which allows to store less information) with a threshold
-%at limitB. When we plot the figure, we plot the complement of binaryImage, because we
-%want black pixels (zeros) to represent the hydride on the figure.
-
+%%%%%%%%%%%%%%%%%%%%%%%%%%%% Binarize the image %%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % Determine and remove the background
 if background_bool
@@ -121,32 +124,34 @@ else
     I3 = I;
 end
 
-
+% Binarize the image using the high and low thresholds
 binaryImage = imcomplement((lowThreshold <= I3) & (I3 <= highThreshold));
-% Remove the black pixels at the borders of the image
+
+% Remove the black pixels at the borders of the image if required
 if removeBorders
     % Fill in the body
     mask = imfill(imcomplement(binaryImage), 'holes'); % Mask is whole solid body.
     % XOR the two images
     binaryImage=xor(mask,imcomplement(binaryImage));
 end
-% Erase the cluster of less than SpotSize pixels (clean the image from isolated
-%groups of pixels)
+
+% Erase the cluster of less than SpotSize pixels
 binaryImage = bwareaopen(binaryImage, SpotSize);
-% fill the small holes in the hydrides
+
+% Fill the holes in smaller than HoleSize pixels
 binaryImage = imcomplement(bwareaopen(imcomplement(binaryImage), HoleSize));
 
-% the hydride is reduced to a curve with a thickness of one pixel. This
-% provides better results for the curvature measurements, and fastens the
-% algorithm because less information is being treated.
+% Thin the hydride to a single pixel
+% This provides better results and fastens the algorithm because less
+% information is being treated.
 binaryImage = bwmorph(binaryImage,'thin',Inf);
-% Erase the cluster of less than Nsize pixels (clean the image from isolated
-%groups of pixels)
+
+% Erase the cluster of less than Nsize pixels
 Nsize2=2;
 binaryImage = bwareaopen(binaryImage, Nsize2);
+
 % thicken the hydrides to make them easier to find for the genetic
 % algorithm
-% binaryImage = bwmorph(binaryImage,'thicken',0);
 se1 = strel('line',2,90);
 se2 = strel('line',2,0);
 binaryImage = imdilate(binaryImage,[se1,se2],'full');
@@ -164,7 +169,6 @@ filename_binary_jpeg=[filename '_binary' '.jpeg'];
 imwrite(imcomplement(binaryImage),filename_binary_jpeg,'jpeg')
 cd ../
 cd(codeFolderName)
-
 % Delete the file that was created
 delete(filename)
 
